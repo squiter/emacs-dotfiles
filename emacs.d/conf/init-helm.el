d6a2c30ebe24dd7ff56faca195fef6b67cfefb5e
Brunno dos Santos <squiter85@gmail.com>
2015-04-09 16:04:11 -0300

    Emacs: Enable helm-mode

--- emacs.d/conf/init-helm.el
+++ emacs.d/conf/init-helm.el
@@ -1,5 +1,7 @@
 (require 'helm-config)
 
+(helm-mode t)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
