710b7a55782392943844a8d01371835fb64862fa
Brunno dos Santos <squiter85@gmail.com>
2015-04-09 16:04:39 -0300

    Emacs: Import my PATH to emacs

--- emacs.d/conf/init-general.el
+++ emacs.d/conf/init-general.el
@@ -9,6 +9,12 @@
  indent-tabs-mode nil
 )
 
+;; import PATH environment variable
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append (split-string-and-unquote path ":") exec-path)))
+
 ;; whitespace display
 (global-whitespace-mode)
 (setq whitespace-global-modes
