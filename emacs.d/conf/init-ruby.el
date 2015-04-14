5b86bd5748201aa4883831d0a7fa40316b0284c9
Brunno dos Santos <squiter85@gmail.com>
2015-04-14 16:31:01 -0300

    Emacs: Add ruby interpolation strings

--- emacs.d/conf/init-ruby.el
+++ emacs.d/conf/init-ruby.el
@@ -65,4 +65,19 @@
   '(progn
      (define-key ruby-mode-map (kbd "C-c , ,") 'senny-ruby-open-spec-other-buffer)))
 
+;; String interpolation
+(defun senny-ruby-interpolate ()
+  "In a double quoted string, interpolate."
+  (interactive)
+  (insert "#")
+  (when (and
+         (looking-back "\".*")
+         (looking-at ".*\""))
+    (insert "{}")
+    (backward-char 1)))
+
+(eval-after-load 'ruby-mode
+  '(progn
+     (define-key ruby-mode-map (kbd "#") 'senny-ruby-interpolate)))
+
 (provide 'init-ruby)
