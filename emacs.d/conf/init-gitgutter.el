48b749f067741f3e47b59dd6a54f955c0017d22f
Brunno dos Santos <squiter85@gmail.com>
2015-04-14 15:43:15 -0300

    Emacs: make magit hook gitgutter

--- emacs.d/conf/init-gitgutter.el
+++ emacs.d/conf/init-gitgutter.el
@@ -6,4 +6,7 @@
 ;; To work well with line numbers
 (git-gutter:linum-setup)
 
+;; hook to update gutter when using magit
+(add-hook 'git-gutter:update-hooks 'magit-revert-buffer-hook)
+
 (provide 'init-gitgutter)
