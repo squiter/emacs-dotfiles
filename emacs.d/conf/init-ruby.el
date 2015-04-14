06aac92c8fb32b938bf5af67a442fbb6507798ea
Brunno dos Santos <squiter85@gmail.com>
2015-04-14 16:29:48 -0300

    Emacs: Add function to goto spec file

--- emacs.d/conf/init-ruby.el
+++ emacs.d/conf/init-ruby.el
@@ -50,4 +50,19 @@
 
 (require 'rubocop)
 
+;;open the spec of a class
+(defun senny-ruby-open-spec-other-buffer ()
+  (interactive)
+  (when (featurep 'rspec-mode)
+    (let ((source-buffer (current-buffer))
+          (other-buffer (progn
+                          (rspec-toggle-spec-and-target)
+                          (current-buffer))))
+      (switch-to-buffer source-buffer)
+      (pop-to-buffer other-buffer))))
+
+(eval-after-load 'ruby-mode
+  '(progn
+     (define-key ruby-mode-map (kbd "C-c , ,") 'senny-ruby-open-spec-other-buffer)))
+
 (provide 'init-ruby)
