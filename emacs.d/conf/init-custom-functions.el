ea19a527b907dcabdb6b5aefa56bc558505694ee
Brunno dos Santos <squiter85@gmail.com>
2015-04-14 14:46:09 -0300

    Emacs: Add smart-init-of-line function

--- emacs.d/conf/init-custom-functions.el
+++ emacs.d/conf/init-custom-functions.el
@@ -5,6 +5,13 @@
   (move-end-of-line 1)
   (newline-and-indent))
 
+(defun custom/smart-move-beginning-of-line ()
+  "Move to beginning of line or to beginning of indentation depending on POINT."
+  (interactive)
+  (if (= (point) (line-beginning-position))
+      (back-to-indentation)
+    (move-beginning-of-line nil)))
+
 "Stackoverflow functions :)"
 (defun switch-fullscreen nil
   (interactive)
--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -1,3 +1,4 @@
 (global-set-key (kbd "C-<return>") 'custom/insert-new-line)
+(global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
 
 (provide 'init-keybindings)
