38282a32840d386bca6def1f9f59d7ae8efb2815
Brunno dos Santos <squiter85@gmail.com>
2015-04-16 13:18:26 -0300

    Emacs: Add simple org configurations

--- emacs.d/conf/init-org.el
+++ emacs.d/conf/init-org.el
@@ -5,4 +5,12 @@
 
 (setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("freela" . ?f)))
 
+;; Standard key bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; Agenda setup
+(setq org-agenda-files (quote ("~/Dropbox/org")))
+
 (provide 'init-org)
