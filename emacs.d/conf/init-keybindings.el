b40e3901c22cfc7f5adca5deb0a65bd5166de7a8
Brunno dos Santos <squiter85@gmail.com>
2015-04-17 22:50:25 -0300

    Emacs: Add keybind to eval-buffer

--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -1,5 +1,6 @@
 (global-set-key (kbd "C-<return>") 'custom/insert-new-line)
 (global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
 (global-set-key (kbd "C-c d") 'custom/duplicate-current-line-or-region)
+(global-set-key (kbd "C-c e") 'eval-buffer)
 
 (provide 'init-keybindings)
