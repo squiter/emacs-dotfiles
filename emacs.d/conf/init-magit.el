4ee8d83ddfbb17b084e70714ec949801af7dcc7e
Brunno dos Santos <squiter85@gmail.com>
2015-05-11 18:09:27 -0300

    Emacs: Show magint in full-screen and after all return to old windows config

--- emacs.d/conf/init-magit.el
+++ emacs.d/conf/init-magit.el
@@ -4,4 +4,18 @@
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "M-g c") 'magit-checkout)
 
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 (provide 'init-magit)
