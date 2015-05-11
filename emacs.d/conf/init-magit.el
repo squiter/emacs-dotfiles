9432168eca0c844ab538aa605d7b12d1b49d272e
Brunno dos Santos <squiter85@gmail.com>
2015-05-11 17:55:45 -0300

    Revert "Emacs: Add quick amend function!"
    This reverts commit b6fd57e21240673a7473c6804d169db3ca33ccb6.

--- emacs.d/conf/init-magit.el
+++ emacs.d/conf/init-magit.el
@@ -4,14 +4,4 @@
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "M-g c") 'magit-checkout)
 
-;; C-c C-a to amend without any prompt
-(defun magit-just-amend ()
-  (interactive)
-  (save-window-excursion
-    (magit-with-refresh
-      (shell-command "git --no-pager commit --amend --reuse-message=HEAD"))))
-
-(eval-after-load "magit"
-  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
-
 (provide 'init-magit)
