fd185a04f69301618f1c5aea23b9e8f29f4dda6e
Brunno dos Santos <squiter85@gmail.com>
2015-05-12 18:11:05 -0300

    Emacs: Add custom colors to reflect evil-mode in mode-line

--- emacs.d/conf/init-evil.el
+++ emacs.d/conf/init-evil.el
@@ -1,4 +1,18 @@
 (require 'evil)
+
 (evil-mode 1)
+
+;; change mode-line color by evil state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+				   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+	    (lambda ()
+	      (let ((color (cond ((minibufferp) default-color)
+				 ((evil-insert-state-p) '("#AB4642" . "#E8E8E8"))
+				 ((evil-emacs-state-p)  '("#444488" . "#E8E8E8"))
+				 ((buffer-modified-p)   '("#006fa0" . "#E8E8E8"))
+				 (t default-color))))
+		(set-face-background 'mode-line (car color))
+		(set-face-foreground 'mode-line (cdr color))))))
 
 (provide 'init-evil)
