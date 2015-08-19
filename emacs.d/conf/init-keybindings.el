d8adbb4a059e66aaa3b4e79dea4977b76db909d7
Brunno dos Santos <squiter85@gmail.com>
2015-08-18 22:38:34 -0300

    Emacs: add more keybindings

--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -1,3 +1,8 @@
+;;; init-keybindings.el --- My custom keybindigs
+;;; Commentary:
+;;  This file doesn't contain specific mode keybindings
+;;; Code:
+
 (global-set-key (kbd "C-<return>") 'custom/insert-new-line)
 (global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
 (global-set-key (kbd "C-c d") 'custom/duplicate-current-line-or-region)
@@ -10,12 +15,9 @@
 (global-set-key (kbd "C-c /") 'custom/toggle-line-comment)
 (global-set-key (kbd "C-h C-m") 'discover-my-major)
 (global-set-key (kbd "C-c j") 'join-line)
-
-;; Custom Keybindings
-(eval-after-load 'orgmode
-  '(define-key (kbd "C-c C-o") 'org-agenda-open-link))
-
-;; Browser ring
+(global-set-key (kbd "C-c w") 'whitespace-cleanup)
 (global-set-key (kbd "s-V") 'helm-show-kill-ring)
+(global-set-key (kbd "C-c i") 'indent-buffer)
 
 (provide 'init-keybindings)
+;;; init-keybindings.el ends here
