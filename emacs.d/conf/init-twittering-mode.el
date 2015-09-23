92f37351a26d42fda2fd8a34ddc0871b616674e1
Brunno dos Santos <squiter85@gmail.com>
2015-09-23 19:46:52 -0300

    Emacs: Sets twitter image size

--- emacs.d/conf/init-twittering-mode.el
+++ emacs.d/conf/init-twittering-mode.el
@@ -8,6 +8,7 @@
 
 (setq twittering-icon-mode t)
 (global-set-key (kbd "C-, t") 'twittering-update-status-from-pop-up-buffer)
+(setq twittering-convert-fix-size 24)
 
 (provide 'init-twittering-mode)
 ;;; init-twittering-mode.el ends here
