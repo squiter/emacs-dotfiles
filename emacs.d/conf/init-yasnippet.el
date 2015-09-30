f75933fdc27e0d058eb17074fa6542ba4e98278d
Brunno dos Santos <squiter85@gmail.com>
2015-09-30 17:02:32 -0300

    Emacs: disable yasnipet for terminal modes

--- emacs.d/conf/init-yasnippet.el
+++ emacs.d/conf/init-yasnippet.el
@@ -7,5 +7,9 @@
 
 (yas-global-mode 1)
 
+;; disable yasnipet for any terminal mode
+(add-hook 'term-mode-hook (lambda()
+                            (yas-minor-mode -1)))
+
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
