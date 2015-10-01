959d245bf8e085c95b2d2320e4b3b47ef45a1192
Brunno dos Santos <squiter85@gmail.com>
2015-10-01 16:40:59 -0300

    Emacs: filter some buffer types from my list
    Removes:
    - org-mode's buffers;
    - org-agenda-mode's buffers;
    - dired-mode's buffers.

--- emacs.d/conf/init-helm.el
+++ emacs.d/conf/init-helm.el
@@ -13,5 +13,17 @@
 (setq helm-quick-update t
      helm-buffers-fuzzy-matching t)
 
+(defun bsl/filter-buffers (buffer-list)
+  (delq nil (mapcar
+             (lambda (buffer)
+               (cond
+                    ((eq (with-current-buffer buffer major-mode)  'dired-mode) nil)
+                    ((eq (with-current-buffer buffer major-mode)  'org-mode) nil)
+                    ((eq (with-current-buffer buffer major-mode)  'org-agenda-mode) nil)
+                    (t buffer)))
+             buffer-list)))
+
+(advice-add 'helm-skip-boring-buffers :filter-return 'bsl/filter-buffers)
+
 (provide 'init-helm)
 ;;; init-helm.el ends here
