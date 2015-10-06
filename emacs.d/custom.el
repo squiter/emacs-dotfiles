403eaec621874a57dd4b74f30480d27359fb51fc
Brunno dos Santos <squiter85@gmail.com>
2015-10-06 10:24:28 -0300

    Emacs: add faces to orgmode in Material theme

---
+++ emacs.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit nil :background "#455A64" :foreground "#eceff1" :overline "#b0bec5" :weight bold :height 1.1))))
+ '(org-level-2 ((t (:inherit nil :background "#35575b" :foreground "#e1f5fe" :overline "#65757e" :height 1.1))))
+ '(org-level-3 ((t (:inherit nil :background "#36676c" :foreground "#a5d6a7" :overline "#65757e" :height 1.1))))
+ '(org-level-4 ((t (:inherit nil :background "#37787d" :foreground "#ffcc80" :overline "#65757e" :height 1.0))))
+ '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
