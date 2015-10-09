30ceccdb4eeca03860b678af7a8ee7e4b2698336
Brunno dos Santos <squiter85@gmail.com>
2015-10-09 10:54:47 -0300

    Emacs: Add `sudo-edit` function

--- emacs.d/conf/init-custom-functions.el
+++ emacs.d/conf/init-custom-functions.el
@@ -168,5 +168,17 @@ downcased, no preceding underscore.
 	  (select-window first-win)
 	  (if this-win-2nd (other-window 1))))))
 
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'init-custom-functions)
 ;;; init-custom-functions.el ends here
--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -3,21 +3,27 @@
 ;;  This file doesn't contain specific mode keybindings
 ;;; Code:
 
-(global-set-key (kbd "C-<return>") 'custom/insert-new-line)
-(global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
+;; C-c prefix
 (global-set-key (kbd "C-c d") 'custom/duplicate-current-line-or-region)
+(global-set-key (kbd "C-c /") 'custom/toggle-line-comment)
+(global-set-key (kbd "C-c j") 'join-line)
+(global-set-key (kbd "C-c w") 'whitespace-cleanup)
+(global-set-key (kbd "C-c i") 'indent-buffer)
 (global-set-key (kbd "C-c e") 'eval-buffer)
+
+;; C-x prefix
 (global-set-key (kbd "C-x -") 'hsplit-last-buffer)
 (global-set-key (kbd "C-x |") 'vsplit-last-buffer)
 (global-set-key (kbd "C-x =") 'swap-buffers-in-windows)
 (global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
 (global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
-(global-set-key (kbd "C-c /") 'custom/toggle-line-comment)
+(global-set-key (kbd "C-x !") 'sudo-edit)
+
+;; Other prefix
 (global-set-key (kbd "C-h C-m") 'discover-my-major)
-(global-set-key (kbd "C-c j") 'join-line)
-(global-set-key (kbd "C-c w") 'whitespace-cleanup)
 (global-set-key (kbd "s-V") 'helm-show-kill-ring)
-(global-set-key (kbd "C-c i") 'indent-buffer)
+(global-set-key (kbd "C-<return>") 'custom/insert-new-line)
+(global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
 
 (provide 'init-keybindings)
 ;;; init-keybindings.el ends here
