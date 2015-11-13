a564cae8380820457232251be14a6358282b3949
Brunno dos Santos <squiter85@gmail.com>
2015-11-13 19:37:58 -0200

    Emacs: add @milhouse style of window navigation

--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -25,5 +25,16 @@
 (global-set-key (kbd "C-<return>") 'custom/insert-new-line)
 (global-set-key (kbd "C-a") 'custom/smart-move-beginning-of-line)
 
+;; window and buffer manipulation
+(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "M-i") 'other-frame)
+(global-set-key (kbd "M-0") 'delete-window)
+(global-set-key (kbd "M-k") 'kill-buffer)
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-2") 'split-window-vertically)
+(global-set-key (kbd "M-3") 'split-window-horizontally)
+(global-set-key (kbd "M-4") 'delete-other-windows)
+(global-set-key (kbd "M-5") 'delete-window)
+
 (provide 'init-keybindings)
 ;;; init-keybindings.el ends here
