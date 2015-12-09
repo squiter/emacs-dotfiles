a9f75a4183fc97bbf35a3997bdea7ba5a24adc39
Brunno dos Santos <squiter85@gmail.com>
2015-12-09 17:11:30 -0200

    Fix my stupi idea with init-path LoL

--- emacs.d/conf/init-path.el
+++ emacs.d/conf/init-path.el
@@ -5,16 +5,17 @@
 ;;; Code:
 
 ;; if OSX
-(if (eq system-type 'darwin)
-    (defun set-exec-path-from-shell-PATH ()
-      "Function that set $PATH env var."
-      (interactive)
-      (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-        (setenv "PATH" path-from-shell)
-        (setq exec-path (split-string path-from-shell path-separator))))
 
-  (when (window-system)
-    (set-exec-path-from-shell-PATH)))
+(defun set-exec-path-from-shell-PATH ()
+  "Function that set $PATH env var."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(if (eq system-type 'darwin)
+    (when (window-system)
+      (set-exec-path-from-shell-PATH)))
 
 (provide 'init-path)
 ;;; init-path.el ends here
