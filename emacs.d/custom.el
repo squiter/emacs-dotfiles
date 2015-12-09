03aeb916a9b94ce85fa8f7664ba2cc719b3b4626
Brunno dos Santos <squiter85@gmail.com>
2015-12-09 17:06:52 -0200

    Custons settings for Wakatime

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(wakatime-cli-path "/usr/local/bin/wakatime")
+ '(wakatime-python-bin "/usr/bin/python"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
