4a187ca1bebbb73eae0de35cec8340e849814144
Brunno dos Santos <squiter85@gmail.com>
2015-12-09 17:02:46 -0200

    Set PATH configurations only for OSX

--- emacs.d/conf/init-path.el
+++ emacs.d/conf/init-path.el
@@ -4,15 +4,17 @@
 ;;  This code solve that kind of problems
 ;;; Code:
 
-(defun set-exec-path-from-shell-PATH ()
-  "Function that set $PATH env var."
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+;; if OSX
+(if (eq system-type 'darwin)
+    (defun set-exec-path-from-shell-PATH ()
+      "Function that set $PATH env var."
+      (interactive)
+      (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+        (setenv "PATH" path-from-shell)
+        (setq exec-path (split-string path-from-shell path-separator))))
 
-(when (window-system)
-  (set-exec-path-from-shell-PATH))
+  (when (window-system)
+    (set-exec-path-from-shell-PATH)))
 
 (provide 'init-path)
 ;;; init-path.el ends here
