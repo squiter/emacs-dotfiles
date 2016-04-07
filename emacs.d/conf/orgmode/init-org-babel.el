55576cef3cfe8ae044e396b1827bfc76a71a79c2
Brunno dos Santos <squiter85@gmail.com>
2016-04-07 10:49:42 -0300

    Set `org-babel` to work with `cider`

--- emacs.d/conf/orgmode/init-org-babel.el
+++ emacs.d/conf/orgmode/init-org-babel.el
@@ -6,6 +6,9 @@
 (setq org-babel-sh-command "bash"
       org-export-babel-evaluate nil)
 
+(setq org-babel-clojure-backend 'cider)
+(require 'cider)
+
 (setq org-src-fontify-natively t)
 (org-babel-do-load-languages'org-babel-load-languages
  '((emacs-lisp . t)
