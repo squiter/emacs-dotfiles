8dd5fb92dbae1dbad2c3eb58f0dbf1bae7cf2223
Brunno dos Santos <squiter85@gmail.com>
2016-04-29 14:25:35 -0300

    Little fix in telephone line colors

--- emacs.d/conf/init-telephone.el
+++ emacs.d/conf/init-telephone.el
@@ -12,7 +12,7 @@
 (set-face-attribute 'mode-line-inactive nil
                     :box nil
                     :foreground "#6272a4"
-                    :background "#282a36")
+                    :background "#2b2f3a")
 
 (set-face-attribute 'mode-line nil
                     :box '(:line-width -1 :style raised)
