87959dd02092226b4e21ea648c9870d8cbdfa1bd
Brunno dos Santos <squiter85@gmail.com>
2016-05-31 15:31:26 -0300

    Add function to copy branch changelog

--- emacs.d/conf/init-magit.el
+++ emacs.d/conf/init-magit.el
@@ -24,5 +24,15 @@
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
+;; stolen from https://github.com/p-lambert/emacs-dotfiles
+(defun plambert/branch-changelog ()
+  (interactive)
+  (let ((default-directory (magit-toplevel))
+        (cmd "git log --format='* %s' origin/master..HEAD"))
+    (kill-new (shell-command-to-string cmd))
+    (message "Changelog copied to kill-ring.")))
+
+(global-set-key (kbd "M-n b c") 'plambert/branch-changelog)
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
