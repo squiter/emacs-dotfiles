421409e48946a2a229ed52a144b39a69d029b6b2
Brunno dos Santos <squiter85@gmail.com>
2016-06-02 15:18:55 -0300

    Using flycheck only in progmode

--- emacs.d/conf/init-flycheck.el
+++ emacs.d/conf/init-flycheck.el
@@ -1,12 +1,9 @@
 ;;; init-flycheck --- Configs for Flycheck plugin
 ;;; Commentary:
 ;;; Code:
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'prog-mode-hook 'flycheck-mode)
 
 (setq-default flycheck-disabled-checkers '(html-tidy))
-
-;; disable flycheck in restclient responses
-(add-hook 'restclient-mode-hook (flycheck-mode 0))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
