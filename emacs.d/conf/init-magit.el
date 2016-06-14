811f1e6f70c9d2625c7d9bf788cc0785589cddc7
Brunno dos Santos <squiter85@gmail.com>
2016-06-14 15:24:30 -0300

    Add `squiter/visit-pull-request-url` for Github, Bitbucket and Locaweb Gitlab

--- emacs.d/conf/init-magit.el
+++ emacs.d/conf/init-magit.el
@@ -34,5 +34,47 @@
 
 (global-set-key (kbd "M-n b c") 'plambert/branch-changelog)
 
+(defun squiter/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (let ((repo (magit-get "remote" (magit-get-remote) "url")))
+    (cond ((string-match "github\\.com" repo)
+           (visit-gh-pull-request repo))
+          ((string-match "bitbucket\\.org" repo)
+           (visit-bb-pull-request repo))
+          ((string-match "locaweb\\.com\\.br" repo)
+           (visit-lw-pull-request repo)))))
+
+;; TODO: check why this functions is not working
+(defun visit-gh-pull-request (repo)
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            repo)
+           (magit-get-current-branch))))
+
+
+(defun visit-bb-pull-request (repo)
+  (browse-url
+   (format "https://bitbucket.org/%s/pull-request/new"
+           (replace-regexp-in-string
+            "\\`.+bitbucket\\.org:\\(.+\\)\\.git\\'" "\\1"
+            repo))))
+
+(defun visit-lw-pull-request (repo)
+  (browse-url
+   (format "https://code.locaweb.com.br/%s/merge_requests/new"
+           (replace-regexp-in-string
+            "\\`.+code\\.locaweb\\.com\\.br:\\(.+\\)\\.git\\'" "\\1"
+            repo))))
+
+;; visit PR for github or bitbucket repositories with "v"
+(eval-after-load 'magit
+  '(define-key magit-mode-map "v"
+     #'squiter/visit-pull-request-url))
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
