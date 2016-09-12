18773dc4d5685fa9b49a8afd6cfff3b51ccad7c4
Brunno dos Santos <squiter85@gmail.com>
2016-09-12 18:55:26 -0300

    Fix anoying highligh of smartparens with doom theme

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -16,3 +16,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button))))))
+ '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))))
+ '(sp-pair-overlay-face ((t (:inherit underline)))))
