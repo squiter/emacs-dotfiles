84ebdb1a9be21415a884858aeb26fc28dab09c50
Brunno dos Santos <squiter85@gmail.com>
2016-10-21 13:49:14 -0200

    Fix Wakatime mode

--- emacs.d/conf/init-wakatime.el
+++ emacs.d/conf/init-wakatime.el
@@ -1,6 +1,35 @@
+;;; init-wakatime.el --- Wakatime configuration
+;;
+;; Copyright (C) 2016 Brunno dos Santos <emacs at brunno dot me>
+;;
+;; Author: Brunno dos Santos @squiter
+;; URL: http://github.com/squiter/emacs-dotfiles
+;;
+;; Created: 21 outubro 2016
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; See <http://www.gnu.org/licenses/> for a copy of the GNU General
+;; Public License.
+;;
+;;; Commentary:
+;;
+
+;;; Code:
 (setq wakatime-api-key "84e8b465-1324-4e75-8109-6c6c4a1d3dee")
 (setq wakatime-cli-path "/usr/local/bin/wakatime")
 
 (global-wakatime-mode)
 
 (provide 'init-wakatime)
+;;; init-wakatime.el ends here
