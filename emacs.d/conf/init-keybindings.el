1c02468d55bee9bc46fb5d8f8a8caf28d60eb14c
Brunno dos Santos <squiter85@gmail.com>
2016-12-07 18:39:56 -0200

    Add keybind: `C-x j` to jump to word

--- emacs.d/conf/init-keybindings.el
+++ emacs.d/conf/init-keybindings.el
@@ -63,6 +63,7 @@
 
 ;; avy
 (global-set-key (kbd "C-x a f") 'avy-goto-line)
+(global-set-key (kbd "C-x j") 'avy-goto-char-timer)
 (global-set-key (kbd "C-x a w") 'avy-goto-word-1)
 (global-set-key (kbd "C-x a e") 'avy-goto-word-0)
 
