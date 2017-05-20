0172544d9e4bc606180454f70839d9246a348631
Brunno dos Santos <squiter85@gmail.com>
2017-05-20 11:39:17 -0300

    Change telephone-line separators

--- emacs.d/conf/init-telephone.el
+++ emacs.d/conf/init-telephone.el
@@ -58,13 +58,13 @@
         (accent . (telephone-line-major-mode-segment
                    telephone-line-position-segment))))
 
-(setq telephone-line-primary-left-separator 'telephone-line-cubed-left
-      telephone-line-left-separator 'telephone-line-cubed-left
-      telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left)
+(setq telephone-line-primary-left-separator 'telephone-line-identity-left
+      telephone-line-left-separator 'telephone-line-identity-left
+      telephone-line-secondary-left-separator 'telephone-line-identity-hollow-left)
 
-(setq telephone-line-primary-right-separator 'telephone-line-cubed-right
-      telephone-line-right-separator 'telephone-line-cubed-right
-      telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+(setq telephone-line-primary-right-separator 'telephone-line-identity-right
+      telephone-line-right-separator 'telephone-line-identity-right
+      telephone-line-secondary-right-separator 'telephone-line-identity-hollow-right)
 
 (telephone-line-mode 1)
 
