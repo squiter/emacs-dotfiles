39b3d7fcb4d8aa2da6abc2c7ba4d8afe648dfa86
Brunno dos Santos <squiter85@gmail.com>
2017-07-03 18:34:05 -0300

    Updating `init-telephone.el` to show Flycheck tips

--- emacs.d/conf/init-telephone.el
+++ emacs.d/conf/init-telephone.el
@@ -37,20 +37,79 @@
                     :background "#20272e")
 
 (telephone-line-defsegment* squiter/telephone-line-buffer-segment ()
-  `("["
-    ,(telephone-line-raw mode-line-modified t)
-    ,"] "
-    ,(telephone-line-raw mode-line-buffer-identification t)))
+  `("",(telephone-line-raw mode-line-buffer-identification t)))
 
 (telephone-line-defsegment* squiter/telephone-line-projectile-project-name ()
   `("📁 " ,(projectile-project-name)))
+
+(defun shackra/vc-state ()
+  (vc-state (buffer-file-name (current-buffer))))
+
+(telephone-line-defsegment* shackra-flycheck-status ()
+  (let* ((text (pcase flycheck-last-status-change
+                 (`finished (if flycheck-current-errors
+                                (let ((count (let-alist (flycheck-count-errors flycheck-current-errors)
+                                               (+ (or .warning 0) (or .error 0)))))
+                                  (format " ✖ %s Issue%s" count (unless (eq 1 count) "s")))
+                              " ✔ No Issues"))
+                 (`running     " ⟲ Working...")
+                 (`no-checker  " ⚠ No Checker")
+                 (`not-checked " ✖ Disabled")
+                 (`errored     " ⚠ Error")
+                 (`interrupted " ⛔ Interrupted")
+                 (`suspicious  ""))))
+    (propertize text
+                'help-echo "Show Flycheck Errors"
+                'mouse-face '(:box 1)
+                'face `(:height 0.8) 'display '(raise 0.1)
+                'local-map (make-mode-line-mouse-map
+                            'mouse-1 (lambda () (interactive) (flycheck-list-errors))))))
+
+(telephone-line-defsegment* shackra-buffer-vc-modified-segment ()
+  (cond ((buffer-modified-p)
+         (propertize
+          (format " %s" (all-the-icons-faicon "pencil"))
+          'face `(:foreground "orange" :height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "Modified buffer."))
+
+        ((eq (shackra/vc-state) 'edited)
+         (propertize
+          (format " %s" (all-the-icons-faicon "pencil"))
+          'face `(:height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "Modified buffer, changes not commited."))
+
+        ((eq (shackra/vc-state) 'unregistered)
+         (propertize
+          (format " %s" (all-the-icons-faicon "question"))
+          'face `(:height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "Archivo sin registrar al VCS."))
+        ((eq (shackra/vc-state) 'missing)
+         (propertize
+          (format " %s" (all-the-icons-faicon "exclamation"))
+          'face `(:height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "File only exists in VCS, not in HD."))
+
+        ((eq (shackra/vc-state) 'ignored)
+         (propertize
+          (format " %s" (all-the-icons-faicon "ban"))
+          'face `(:height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "Ignored file."))
+
+        ((eq (shackra/vc-state) 'added)
+         (propertize
+          (format " %s" (all-the-icons-faicon "plus"))
+          'face `(:height 0.8 :family ,(all-the-icons-faicon-family))
+          'display '(raise 0.1) 'help-echo "Added file."))))
 
 (setq telephone-line-lhs
       '((accent . (squiter/telephone-line-buffer-segment
                    telephone-line-process-segment
                    telephone-line-erc-modified-channels-segment))
-        (nil . (telephone-line-vc-segment
-                squiter/telephone-line-projectile-project-name))))
+        (nil . (telephone-line-process-segment
+                shackra-buffer-vc-modified-segment
+                squiter/telephone-line-projectile-project-name
+                shackra-flycheck-status
+                ))))
 
 (setq telephone-line-rhs
       '((nil    . (telephone-line-misc-info-segment))
