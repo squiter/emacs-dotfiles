0c512964c68e8c638a134af99bc54f631247b864
Brunno dos Santos <brunno@brunno.me>
2018-12-06 17:45:58 -0200

    Add `clj-refactor`

--- emacs.d/conf/init-clojure.el
+++ emacs.d/conf/init-clojure.el
@@ -114,5 +114,15 @@
 ;; (use-package flycheck-clojure
 ;;   :config (eval-after-load 'flycheck '(flycheck-clojure-setup)))
 
+(use-package clj-refactor
+  :init
+  (defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+  :config (add-hook 'clojure-mode-hook #'my-clojure-mode-hook))
+
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
