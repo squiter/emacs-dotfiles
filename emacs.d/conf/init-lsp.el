92ca232cc8596ca1af5262b40485efd0f47ef3a2
Brunno dos Santos <brunno@brunno.me>
2020-07-07 17:55:32 -0300

    Changing the configuration of `lsp-ui-doc`

--- emacs.d/conf/init-lsp.el
+++ emacs.d/conf/init-lsp.el
@@ -40,11 +40,7 @@
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (setq lsp-ui-sideline-enable t
-        lsp-ui-sideline-update-mode 'line
-        lsp-ui-sideline-show-code-actions t
-        lsp-ui-doc-position "top"
-        lsp-ui-sideline-show-hover t
+  (setq lsp-ui-doc-position "top"
         lsp-ui-flycheck-enable t
         lsp-ui-imenu-enable t
         lsp-ui-sideline-ignore-duplicate t))
