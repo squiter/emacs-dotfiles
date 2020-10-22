56a1e5cc406f7c6f591d4e7bf198f48909dd373d
Brunno dos Santos <brunno@brunno.me>
2020-10-22 09:13:40 -0300

    Creating `early-init.el` - Yes! Emacs 27!

--- emacs.d/conf/init-ui.el
+++ emacs.d/conf/init-ui.el
@@ -9,24 +9,6 @@
 (load-theme 'challenger-deep t)
 ;; (load-theme 'apropospriate-light t)
 
-(setq
- ;; better startup
- inhibit-splash-screen t
- inhibit-startup-message t
- ;; show column number at bottom bar
- column-number-mode t
- ;; disable anoying beep
- ring-bell-function 'ignore
- ;; improve rendering performance
- redisplay-dont-pause t
- )
-
-;; other options
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(set-face-attribute 'default nil :height 140 :font "Ubuntu Mono")
-
 (global-display-line-numbers-mode)
 
 ;; Highlight current line
---
+++ emacs.d/early-init.el
@@ -0,0 +1,26 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. Doom handles package initialization, so
+;; we must prevent Emacs from doing it early!
+;; Copied from https://github.com/hlissner/doom-emacs/blame/develop/early-init.el
+(setq package-enable-at-startup nil)
+(fset #'package--ensure-init-file #'ignore)
+
+(setq
+ ;; better startup
+ inhibit-splash-screen t
+ inhibit-startup-message t
+ ;; show column number at bottom bar
+ column-number-mode t
+ ;; disable anoying beep
+ ring-bell-function 'ignore
+ ;; improve rendering performance
+ redisplay-dont-pause t
+ )
+
+;; other options
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(set-face-attribute 'default nil :height 140 :font "Ubuntu Mono")
