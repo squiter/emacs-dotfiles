24f25eee4b8250da7156c5aa57dc766f4cf30a04
Brunno dos Santos <brunno@brunno.me>
2021-02-24 18:26:58 -0300

    Making `eshell` loading lazily

--- emacs.d/conf/init-eshell.el
+++ emacs.d/conf/init-eshell.el
@@ -1,36 +1,36 @@
 ;;; init-eshell.el --- My configuration for Emacs Shell
 ;;; Commentary:
 ;;; Code:
-(require 'eshell)
+(use-package eshell
+  :commands (eshell custom/projectile-eshell)
+  :bind ("C-c C-t" . 'custom/projectile-eshell)
+  :config
+  (defun custom/bash-command (&rest cmd)
+    "Run CMD as if you were in a bash shell instead of Eshell."
+    (insert (format "bash -c 'source ~/.bash_profile; cd %s; %s'"
+                    (eshell/pwd)
+                    (mapconcat 'identity (car cmd) " ")))
+    (eshell-send-input))
 
-(defun custom/bash-command (&rest cmd)
-  "Run CMD as if you were in a bash shell instead of Eshell."
-  (insert (format "bash -c 'source ~/.bash_profile; cd %s; %s'"
-                  (eshell/pwd)
-                  (mapconcat 'identity (car cmd) " ")))
-  (eshell-send-input))
+  (defun custom/projectile-eshell ()
+    "Open an eshell buffer at project's root directory."
+    (interactive)
+    (let ((shell-dir (projectile-project-root))
+          (shell-title (format "*eshell [%s]*" (projectile-project-name))))
+      (other-window 1)
+      (if (get-buffer shell-title)
+          (switch-to-buffer (get-buffer shell-title))
+        (switch-to-buffer (generate-new-buffer shell-title))
+        (eshell-mode)
+        (goto-char (point-max))
+        (insert (format "cd %s" shell-dir))
+        (eshell-send-input))))
 
-(defun custom/projectile-eshell ()
-  "Open an eshell buffer at project's root directory."
-  (interactive)
-  (let ((shell-dir (projectile-project-root))
-        (shell-title (format "*eshell [%s]*" (projectile-project-name))))
-    (other-window 1)
-    (if (get-buffer shell-title)
-        (switch-to-buffer (get-buffer shell-title))
-      (switch-to-buffer (generate-new-buffer shell-title))
-      (eshell-mode)
-      (goto-char (point-max))
-      (insert (format "cd %s" shell-dir))
-      (eshell-send-input))))
-
-(global-set-key (kbd "C-c C-t") 'custom/projectile-eshell)
-
-;;; Emacs Shell Alias
-(require 'em-alias)
-(add-to-list 'eshell-command-aliases-list (list "ls" "ls -l"))
-(add-to-list 'eshell-command-aliases-list (list "be" "bundle exec $*"))
-(add-to-list 'eshell-command-aliases-list (list "fuck_product" "RAILS_ENV=test rake db:drop db:create db:migrate db:seed"))
+   ;;; Emacs Shell Alias
+  (require 'em-alias)
+  (add-to-list 'eshell-command-aliases-list (list "ls" "ls -l"))
+  (add-to-list 'eshell-command-aliases-list (list "be" "bundle exec $*"))
+  (add-to-list 'eshell-command-aliases-list (list "fuck_product" "RAILS_ENV=test rake db:drop db:create db:migrate db:seed")))
 
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
