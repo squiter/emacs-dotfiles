76aab892a6577f7d896f1356b7b3f1a31251a13f
Brunno dos Santos <brunno@brunno.me>
2021-11-08 19:50:58 -0300

    Add `rust-mode`

--- emacs.d/conf/init-simple-packages.el
+++ emacs.d/conf/init-simple-packages.el
@@ -147,6 +147,7 @@
 (use-package shades-of-purple-theme)
 
 (use-package scala-mode :interpreter ("scala" . scala-mode))
+(use-package rust-mode)
 
 (provide 'init-simple-packages)
 ;;; init-simple-packages.el ends here
