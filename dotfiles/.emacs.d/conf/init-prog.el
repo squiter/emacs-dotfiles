f9cc67afa4c3c968edc69c198a4224daeb8e3a25
Brunno dos Santos <brunno.dossantosleite@remote.com>
2025-01-23 10:11:26 -0300

    Adding support to Elixir

--- dotfiles/.emacs.d/conf/init-prog.el
+++ dotfiles/.emacs.d/conf/init-prog.el
@@ -35,5 +35,40 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+(use-package emacs
+  :ensure nil
+  :custom
+
+  ;; Should use:
+  ;; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+  ;; at least once per installation or while changing this list
+  (treesit-language-source-alist
+   '((heex "https://github.com/phoenixframework/tree-sitter-heex")
+     (elixir "https://github.com/elixir-lang/tree-sitter-elixir")))
+
+  (major-mode-remap-alist
+   '((elixir-mode . elixir-ts-mode))))
+
+(use-package eglot
+ :ensure nil
+ :config (add-to-list 'eglot-server-programs '(elixir-ts-mode "language_server.sh")))
+
+(use-package elixir-ts-mode
+ :hook (elixir-ts-mode . eglot-ensure)
+ (elixir-ts-mode
+  .
+  (lambda ()
+    (push '(">=" . ?\u2265) prettify-symbols-alist)
+    (push '("<=" . ?\u2264) prettify-symbols-alist)
+    (push '("!=" . ?\u2260) prettify-symbols-alist)
+    (push '("==" . ?\u2A75) prettify-symbols-alist)
+    (push '("=~" . ?\u2245) prettify-symbols-alist)
+    (push '("<-" . ?\u2190) prettify-symbols-alist)
+    (push '("->" . ?\u2192) prettify-symbols-alist)
+    (push '("<-" . ?\u2190) prettify-symbols-alist)
+    (push '("|>" . ?\u25B7) prettify-symbols-alist)))
+ (before-save . eglot-format))
+
+
 (provide 'init-prog)
 ;; init-prog.el ends here
