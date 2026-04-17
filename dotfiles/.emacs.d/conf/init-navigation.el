7bbfeb3e8e2af1d5277cf9f72995289b6ca669b8
Brunno dos Santos <brunno.dossantosleite@remote.com>
2026-04-17 18:16:08 -0300

    [remote] Fixing some other errors when running tiger

--- dotfiles/.emacs.d/conf/init-navigation.el
+++ dotfiles/.emacs.d/conf/init-navigation.el
@@ -139,8 +139,6 @@
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep consult-man
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
 
@@ -291,6 +289,8 @@ targets."
       root))
 
   (advice-add 'projectile-project-root :filter-return #'custom/projectile-tiger-root-fix)
+
+  (setq projectile-git-submodule-command  nil)
 
   (defun custom/copy-relative-path-to-file()
     (interactive)
